4a4958f15f4ed438425609153596548ab0e75460
Patel Rajan <76256243+PatelRajan-2481@users.noreply.github.com>
2025-04-11 22:04:29 -0400

    Update config.tf

--- environments/staging/config.tf
+++ environments/staging/config.tf
@@ -1,4 +1,15 @@
 terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+terraform {
   backend "s3" {
     bucket  = "acs730-rajan-stag-tfstate"
     key     = "terraform.tfstate"
