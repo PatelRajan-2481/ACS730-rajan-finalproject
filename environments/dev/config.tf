104a9bab5fa794db6266d16cfac8098bead3ef79
Patel Rajan <76256243+PatelRajan-2481@users.noreply.github.com>
2025-04-11 22:05:01 -0400

    Update config.tf

--- environments/dev/config.tf
+++ environments/dev/config.tf
@@ -1,4 +1,14 @@
 terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+terraform {
   backend "s3" {
     bucket  = "acs730-rajan-dev-tfstate"
     key     = "terraform.tfstate"
